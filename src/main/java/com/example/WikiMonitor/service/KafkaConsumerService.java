5c5837e2a1dbe82e39377c3d531f56aa48ab0dad
Aleksey Bondarev <aleksey.bondarev@csdevents.com>
2026-06-21 02:13:49 +0700

    project init

---
+++ src/main/java/com/example/WikiMonitor/service/KafkaConsumerService.java
@@ -0,0 +1,4 @@
+package com.example.WikiMonitor.service;
+
+public class KafkaConsumerService {
+}
---
+++ src/test/java/com/example/WikiMonitor/WikiMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WikiMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
